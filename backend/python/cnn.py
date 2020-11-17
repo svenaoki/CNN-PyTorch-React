896f600ddf743ecac55f466f1d6fb5f2045986bc
Sven Schilke <sven.schilke@ch.abb.com>
2020-11-17 15:53:48 +0100

    python scripts

--- backend/python/cnn.py
+++ backend/python/cnn.py
@@ -1,15 +1,13 @@
+import math
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
-from PIL import Image
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 import matplotlib.pyplot as plt
-import os
 from convNetModel import convNet
 
-model = convNet()
-print(model)
+
 TRAIN_DIR = "./dataset/train"
 TEST_DIR = "./dataset/test"
 CLASSES = 'Cat', 'Dog'
@@ -35,5 +33,50 @@ inputs, label = train_iter.next()
 for i in range(BATCH_SIZE):
     plt.subplot(2, 3, i+1)
     plt.imshow(inputs[i][0])
-    plt.title(CLASSES[label[i]])
+    plt.title(label[i])
 # plt.show()
+
+# hyperparamters
+NUM_EPOCHS = 2
+ITER_PER_EPOCH = math.ceil(len(train_set)/BATCH_SIZE)
+LEARNING_RATE = 0.01
+
+model = convNet().to(device)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
+criterion = nn.BCELoss()
+
+n_correct = 0
+n_samples = 0
+model.train()
+for epoch in range(NUM_EPOCHS):
+    for i, (features, label) in enumerate(train_loader):
+        features, label = features.to(
+            device), label.to(torch.float32).to(device)
+        predictions = model(features)
+        predictions = predictions.view(-1).to(torch.float32)
+        loss = criterion(predictions, label)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        prediction_cls = predictions < 0.5
+        n_correct += (prediction_cls == label).sum().item()
+        n_samples += label.shape[0]
+        if i % 300 == 0:
+            print(
+                f'Epoch: {epoch+1}/{NUM_EPOCHS}, Iteration: {i+1}/{ITER_PER_EPOCH} Accuracy: {n_correct/n_samples:.4%}')
+
+n_correct = 0
+n_samples = 0
+model.eval()
+with torch.no_grad():
+    for i, (features, label) in enumerate(test_loader):
+        features = features.to(device)
+        label = label.to(device)
+        predictions = model(features)
+        predictions = predictions.view(-1)
+        prediction_cls = predictions < 0.5
+        n_correct += (prediction_cls == label).sum().item()
+        n_samples += label.shape[0]
+    print(f'Accuracy: {n_correct/n_samples:.4%}')
--- backend/python/convNetModel.py
+++ backend/python/convNetModel.py
@@ -1,14 +1,19 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 # (W - F *2P)/S + 1
-# (128 - 5)/1 + 1 = 6xx
-# (376 - 2)/2 + 1 = 6xx
-# (188 - 3) + 1 = 16xx
-# (186 - 2)/2 + 1 = 16xx
-# (93 + 3) + 1 = 16xx
-# (91 - 2)/2 + 1 = 16x46x46
-#
+# l1 (3,6,5) + pool
+# (128 - 5)/1 + 1 = 6x124x124
+# (124 - 2)/2 + 1 = 6x62x62
+
+# l2 (6,16,3) + pool
+# (62 - 3) + 1 = 16x60x60
+# (60 - 2)/2 + 1 = 16x30x30
+
+# l3 (16,6,3) + pool
+# (29 - 3) + 1 = 6x27x27
+# (27 - 2)/2 + 1 = 6x14x14
 
 
 class convNet(nn.Module):
@@ -17,4 +22,17 @@ class convNet(nn.Module):
         self.l1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.l2 = nn.Conv2d(6, 16, 3)
-        self.l3 = nn.Conv2d(6, 16, 3)
+        self.l3 = nn.Conv2d(16, 6, 3)
+        self.f1 = nn.Linear(6*14*14, 120)
+        self.f2 = nn.Linear(120, 84)
+        self.f3 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.l1(x)))
+        x = self.pool(F.relu(self.l2(x)))
+        x = self.pool(F.relu(self.l3(x)))
+        x = x.view(-1, 6*14*14)
+        x = F.relu(self.f1(x))
+        x = F.relu(self.f2(x))
+        x = self.f3(x)
+        return torch.sigmoid(x)
